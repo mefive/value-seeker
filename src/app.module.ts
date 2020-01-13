9332e5d54206c958265ec83bca4218c77aadb56b
liucong211463 <liucong211463@sogou-inc.com>
2020-01-13 22:58:38 +0800

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { IndexBasicModule } from './index-basic/index-basic.module';
+import { IndexDailyModule } from './index-daily/index-daily.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     IndexBasicModule,
     Logger,
+    IndexDailyModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/index-daily/index-daily.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '../base/base.entity';
+
+@Entity('index_daily')
+export class IndexDailyEntity extends BaseEntity {
+  @Column()
+  tsCode: string = '';
+
+  @Column()
+  tradeDate: string = '';
+
+  @Column()
+  close: number = 0;
+
+  @Column()
+  open: number = 0;
+
+  @Column()
+  high: number = 0;
+
+  @Column()
+  low: number = 0;
+
+  @Column()
+  change: number = 0;
+
+  @Column()
+  pctChange: number = 0;
+
+  @Column()
+  vol: number = 0;
+
+  @Column()
+  amount: number = 0;
+}
